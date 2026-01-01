7e2fc907be893b5929fcb9cc3d0ea24e45223e43
jebb <gabouchard2002@gmail.com>
2026-01-01 22:10:18 +0300

    bug fix

--- src/models/dict_models.py
+++ src/models/dict_models.py
@@ -1,7 +1,7 @@
 from datetime import datetime, date
-from typing import Optional
+from typing import Optional, Union
 
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, model_validator, field_validator
 
 from src.config import config
 
@@ -17,28 +17,23 @@ class Word(BaseModel):
     part_of_speech: Optional[str] = Field(None, description="Часть речи слова")
     translation: Optional[str] = Field(None, description="Перевод слова")
     is_public: bool = Field(False, description="Видно ли слово остальным пользователям")
-    created_at: Optional[str] = Field(None, description="Время создания карточки со словом")
+    created_at:Union[str, datetime] = Field(None, description="Время создания карточки со словом")
     context: Optional[str] = Field(None, description="Контекст к слову")
     audio: Optional[bytes] = Field(None, description="bytes of audio recording")
 
     @classmethod
-    @model_validator(mode='before')
-    def set_datetime_to_string(cls, values):
-        try:
-            created_at = values['created_at']
-            if isinstance(created_at, datetime):
-                values['created_at'] = values['created_at'].date().isoformat()
+    @field_validator('created_at', mode='before')
+    def set_datetime_to_string(cls, value):
+        if isinstance(value, datetime):
+            value = value.date().isoformat()
+        elif isinstance(value, str):
+            try:
+                datetime.fromisoformat(value)
+            except ValueError:
+                value = datetime.now(tz=config.tz_info).isoformat()
 
-            elif isinstance(created_at, date):
-                values['created_at'] = values['created_at'].isoformat()
+        return value
 
-            return values
-
-        except KeyError:
-            values.update(
-                created_at=datetime.now(tz=config.tz_info).isoformat()
-            )
-            return values
 
 
 class Stats(BaseModel):
